cbfc7a9e9616908ccdf8ed2622e664efb4be5464
aditya prasad <adityaprasad85470@gmail.com>
2025-10-15 15:42:26 +0530

    Add initial implementations for hello_world, java_day2, and string classes

---
+++ java_day2.java
@@ -0,0 +1,15 @@
+class java_day2 {
+public static void main(String args[]) 
+    {
+ String item = "Soap";
+ int quantity=4;
+ double unit_price=18.75;
+
+ double total_price=quantity*unit_price;
+
+    System.out.println("Item: "+item);
+    System.out.println("Quantity: "+quantity);
+    System.out.println("Unit Price: "+unit_price);
+    System.out.println("Total Price: "+total_price);
+    }
+}
